2fcf9f520f7d2f05f2f3435052fb976d201368ab
Luiz Paulo <luiz.paulo.neto@hotmail.com>
2014-09-03 16:41:06 -0300

    Update mainwindow.cpp

--- Luiz-Paulo/mainwindow.cpp
+++ Luiz-Paulo/mainwindow.cpp
@@ -17,3 +17,8 @@ void MainWindow::on_actionCadastrar_Professor_triggered()
 {
   QMessageBox::information(this, "Cadastro de professor", "Tela de teste de cadastro");
 }
+
+void MainWindow::on_actionCadastrar_Turma_triggered()
+{
+  QMessageBox::information(this, "Cadastro de Turma", "Tela de teste de cadastro");
+}
