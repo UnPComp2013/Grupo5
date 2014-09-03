74cdaab0dbf95bfeda63bf3896aa2b5f68145d7c
geovanips <geovanips@outlook.com>
2014-09-03 19:14:37 -0300

    Update mainwindow.cpp

--- Geovani/mainwindow.cpp
+++ Geovani/mainwindow.cpp
@@ -6,9 +6,18 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->actionIncluir_3, SIGNAL(triggered()),this,SLOT(Incluir()));
+
+    ui->frame_InclusaoProf->setVisible(false);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::Incluir()
+{
+    ui->frame_InclusaoProf->setVisible(true);
+    //QMessageBox::information(this,"Cadastrado com sucesso!!","Foi realizado o cadastro");
 }
