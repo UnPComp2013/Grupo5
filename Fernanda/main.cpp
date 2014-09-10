149ec4e56c39e88f08574257fb4559d08547e5cb
fernandaagr <fernanda_de_araujo@hotmail.com>
2014-09-09 23:16:53 -0300

    Update main.cpp

--- Fernanda/main.cpp
+++ Fernanda/main.cpp
@@ -1,10 +1,10 @@
-#include "mainwindow.h"
+#include "forminicio.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    primeira w;
     w.show();
 
     return a.exec();
