d032e98e46ba5eb6f07f705b6181c6bccac3f067
geovanips <geovanips@outlook.com>
2014-09-10 01:37:05 -0300

    Create mainwindow.h

---
+++ Geovani/mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMessageBox>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+public slots:
+    void Botton();
+    void QWidgetWindow();
+
+
+
+
+private slots:
+    void on_sobre_widget_clicked();
+};
+
+#endif // MAINWINDOW_H
+
