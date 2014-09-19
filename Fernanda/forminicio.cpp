e5bb19263d8b41c2df68a4008fa503cd3b540e16
fernandaagr <fernanda_de_araujo@hotmail.com>
2014-09-18 23:14:45 -0300

    Update forminicio.cpp

--- Fernanda/forminicio.cpp
+++ Fernanda/forminicio.cpp
@@ -1,35 +1,35 @@
 #include "forminicio.h"
-#include "ui_primeira.h"
+#include "ui_forminicio.h"
 #include <QMessageBox>
 
 
-primeira::primeira(QWidget *parent) :
+formInicio::formInicio(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::primeira)
+    ui(new Ui::formInicio)
 {
     ui->setupUi(this);
     QPixmap pix("C:/Users/Fernanda/Desktop/agenda3.jpg"); //adicionar a pasta do qt
     ui->label_img->setPixmap(pix);
 }
 
-primeira::~primeira()
+formInicio::~formInicio()
 {
     delete ui;
 }
 
-void primeira::on_btSobre_clicked()
+void formInicio::on_btSobre_clicked()
 {
     QMessageBox::information(this, "Sobre o programa", "Aqui vão estar algumas informações sobre o programa. Quem desenvolvel, porque, ...");
 }
 
-void primeira::on_btAjuda_clicked()
+void formInicio::on_btAjuda_clicked()
 {
 
     QMessageBox::information(this, "Ajuda", "Aqui pode estar a documentação do programa. Tipo um tutorial de como usar.");
 }
 
-void primeira::on_btEntrar_clicked()
+void formInicio::on_btEntrar_clicked()
 {
-    busca = new formulariobusca(this);
-    busca->show();
+    fLogin = new formlogin(this);
+    fLogin->show();
 }
