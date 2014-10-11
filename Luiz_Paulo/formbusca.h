1747b066c201b228d7b7d258d653577a611c62e2
Luiz Paulo <luiz.paulo.neto@hotmail.com>
2014-10-11 14:40:21 -0300

    Update formbusca.h

--- Luiz_Paulo/formbusca.h
+++ Luiz_Paulo/formbusca.h
@@ -1,4 +1,12 @@
-#include "formbusca.h"
-#include "ui_formbusca.h"
-#include <QMessageBox>
-#include <formcadprof.h>
+#ifndef FORMBUSCA_H
+#define FORMBUSCA_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class formbusca;
+}
+
+class formbusca : public QMainWindow
+{
+    Q_OBJECT
