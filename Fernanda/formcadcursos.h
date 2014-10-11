6907b40eaa82891a8a0c166f1b75f1f806b0271f
fernandaagr <fernanda_de_araujo@hotmail.com>
2014-10-11 17:39:19 -0300

    Update formcadcursos.h

--- Fernanda/formcadcursos.h
+++ Fernanda/formcadcursos.h
@@ -6,9 +6,20 @@
 namespace Ui {
 class formcadcursos;
 }
+
 class formcadcursos : public QDialog
 {
     Q_OBJECT
+    
 public:
     explicit formcadcursos(QWidget *parent = 0);
     ~formcadcursos();
+
+private slots:
+    void on_btCancelar_clicked();
+
+    void on_btLimpar_clicked();
+
+    void on_btSalvar_clicked();
+
+
