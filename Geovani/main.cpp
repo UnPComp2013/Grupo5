e36be785ec587bc98a590278f8c9e39d9508b9eb
geovanips <geovanips@outlook.com>
2014-10-13 23:33:17 -0300

    Update main.cpp

--- Geovani/main.cpp
+++ Geovani/main.cpp
@@ -1,9 +1,17 @@
 #include "telalogin.h"
 #include <QApplication>
 
+#include <QtSql/QSql>
+#include <QtDebug>
+
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+     QString servername = "LOCALHOST\\SQLEXPRESS";
+    QString dbname = "organiza";
+    
     telalogin w;
     w.show();
 
