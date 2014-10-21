dcaff80b0bfa199ccd2049e8459ce12be803af42
fernandaagr <fernanda_de_araujo@hotmail.com>
2014-10-21 09:44:16 -0300

    Update formcaddis.h

--- Fernanda/formcaddis.h
+++ Fernanda/formcaddis.h
@@ -1,4 +1,29 @@
 #ifndef FORMCADDIS_H
 #define FORMCADDIS_H
 
+#include <QDialog>
+
+namespace Ui {
+class formcaddis;
+}
+
+class formcaddis : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit formcaddis(QWidget *parent = 0);
+    ~formcaddis();
+
+private slots:
+    void on_btCancelar_clicked();
+
+    void on_btLimpar_clicked();
+
+    void on_btSalvar_clicked();
+
+private:
+    Ui::formcaddis *ui;
+};
+
 #endif // FORMCADDIS_H
