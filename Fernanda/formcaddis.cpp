d58523e90d5b8e0ec8de8498af71f2931bb5016a
fernandaagr <fernanda_de_araujo@hotmail.com>
2014-10-21 09:45:08 -0300

    Update formcaddis.cpp

--- Fernanda/formcaddis.cpp
+++ Fernanda/formcaddis.cpp
@@ -8,3 +8,23 @@ formcaddis::formcaddis(QWidget *parent) :
 {
     ui->setupUi(this);
 }
+formcaddis::~formcaddis()
+{
+    delete ui;
+}
+
+void formcaddis::on_btCancelar_clicked()
+{
+    close();
+}
+
+void formcaddis::on_btLimpar_clicked()
+{
+    ui->leNome->setText("");
+    ui->leCargaH->setText("");
+}
+
+void formcaddis::on_btSalvar_clicked()
+{
+    QMessageBox::information(this, "Sobre dados", "Deseja salvar dados?");
+}
