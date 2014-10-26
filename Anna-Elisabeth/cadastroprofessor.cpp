892de7d99689f06de14a02f43d0741134c30f2cc
annabarata <bebeth_barata@hotmail.com>
2014-10-26 00:12:28 -0200

    Update cadastroprofessor.cpp

--- Anna-Elisabeth/cadastroprofessor.cpp
+++ Anna-Elisabeth/cadastroprofessor.cpp
@@ -1,5 +1,6 @@
 #include "cadastroprofessor.h"
 #include "ui_cadastroprofessor.h"
+#include <QMessageBox>
 
 cadastroprofessor::cadastroprofessor(QWidget *parent) :
     QDialog(parent),
@@ -11,4 +12,25 @@ cadastroprofessor::cadastroprofessor(QWidget *parent) :
 cadastroprofessor::~cadastroprofessor()
 {
     delete ui;
+}
+
+void cadastroprofessor::on_btCancelar_clicked()
+{
+    close();
+}
+
+void cadastroprofessor::on_btLimpar_clicked()
+{
+    ui->leNome->setText("");
+    ui->leEndereco->setText("");
+    ui->leCPF->setText("");
+    ui->leRG->setText("");
+    ui->leTelefone->setText("");
+    ui->leCelular->setText("");
+    ui->leEmail->setText("");
+}
+
+void cadastroprofessor::on_btSalvar_clicked()
+{
+    QMessageBox::information(this, "Sobre dados", "Deseja salvar dados?");
 }
