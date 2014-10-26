89d74b88884561685f79c51378de7de6ad97afa1
annabarata <bebeth_barata@hotmail.com>
2014-10-26 00:13:49 -0200

    Update sobre.cpp

--- Anna-Elisabeth/sobre.cpp
+++ Anna-Elisabeth/sobre.cpp
@@ -6,9 +6,17 @@ sobre::sobre(QWidget *parent) :
     ui(new Ui::sobre)
 {
     ui->setupUi(this);
+    ui->labelInfo->setText("Informações sobre quem desenvolveu o software, porque, etc,  ....");
+    ui->labelVersao->setText("1.0");
+    ui->labelAno->setText("2014");
 }
 
 sobre::~sobre()
 {
     delete ui;
+}
+
+void sobre::on_btOk_clicked()
+{
+    close();
 }
