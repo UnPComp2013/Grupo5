0aeda7ba561d7d6debff9162bc5e3da5e196ed6c
annabarata <bebeth_barata@hotmail.com>
2014-10-26 00:10:56 -0200

    Update cadastrodisciplina.h

--- Anna-Elisabeth/cadastrodisciplina.h
+++ Anna-Elisabeth/cadastrodisciplina.h
@@ -15,6 +15,13 @@ public:
     explicit cadastrodisciplina(QWidget *parent = 0);
     ~cadastrodisciplina();
 
+private slots:
+    void on_btCancelar_clicked();
+
+    void on_btLimpar_clicked();
+
+    void on_btSalvar_clicked();
+
 private:
     Ui::cadastrodisciplina *ui;
 };
