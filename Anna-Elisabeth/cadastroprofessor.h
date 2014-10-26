84a6894dd8614654b32656b91784f495318ee10c
annabarata <bebeth_barata@hotmail.com>
2014-10-26 00:12:09 -0200

    Update cadastroprofessor.h

--- Anna-Elisabeth/cadastroprofessor.h
+++ Anna-Elisabeth/cadastroprofessor.h
@@ -15,8 +15,16 @@ public:
     explicit cadastroprofessor(QWidget *parent = 0);
     ~cadastroprofessor();
 
+private slots:
+    void on_btCancelar_clicked();
+
+    void on_btLimpar_clicked();
+
+    void on_btSalvar_clicked();
+
 private:
     Ui::cadastroprofessor *ui;
 };
 
 #endif // CADASTROPROFESSOR_H
+
