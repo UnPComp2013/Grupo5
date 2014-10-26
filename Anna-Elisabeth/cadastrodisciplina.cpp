a4dc4bcfa1b9444d3ab6e43297b732820ab06eda
annabarata <bebeth_barata@hotmail.com>
2014-10-26 00:11:22 -0200

    Update cadastrodisciplina.cpp

--- Anna-Elisabeth/cadastrodisciplina.cpp
+++ Anna-Elisabeth/cadastrodisciplina.cpp
@@ -1,6 +1,6 @@
 #include "cadastrodisciplina.h"
 #include "ui_cadastrodisciplina.h"
-
+#include <QMessageBox>
 cadastrodisciplina::cadastrodisciplina(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::cadastrodisciplina)
@@ -11,4 +11,20 @@ cadastrodisciplina::cadastrodisciplina(QWidget *parent) :
 cadastrodisciplina::~cadastrodisciplina()
 {
     delete ui;
+}
+
+void cadastrodisciplina::on_btCancelar_clicked()
+{
+    close();
+}
+
+void cadastrodisciplina::on_btLimpar_clicked()
+{
+    ui->leNome->setText("");
+    ui->leCargaH->setText("");
+}
+
+void cadastrodisciplina::on_btSalvar_clicked()
+{
+    QMessageBox::information(this, "Sobre dados", "Deseja salvar dados?");
 }
