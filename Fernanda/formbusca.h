13578351d4c7429d0f8455bf70886ad51c770e9d
fernandaagr <fernanda_de_araujo@hotmail.com>
2014-11-05 10:13:05 -0300

    Update formbusca.h

--- Fernanda/formbusca.h
+++ Fernanda/formbusca.h
@@ -16,20 +16,21 @@ public:
     ~formbusca();
 
 private slots:
-    void on_actionCadastro_Professor_triggered();
+    void on_btLimpar_clicked();
+
+    void on_btBuscar_clicked();
+
+    void on_actionCadastro_de_Professor_triggered();
+
+    void on_actionCadastro_de_Curso_triggered();
 
     void on_actionCadastro_de_Disciplina_triggered();
 
     void on_actionCadastro_de_Turma_triggered();
 
-    void on_actionCadastro_de_Hor_rio_triggered();
-
-    void on_btLimpar_clicked();
-
-    void on_btBuscar_clicked();
-
 private:
     Ui::formbusca *ui;
+    
 };
 
 #endif // FORMBUSCA_H
