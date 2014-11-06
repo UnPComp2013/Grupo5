eb5cebebc19625f045c9f18a7d535e4bcc92bb88
annabarata <bebeth_barata@hotmail.com>
2014-11-06 00:55:21 -0200

    Update telainicio.h

--- Anna-Elisabeth/telainicio.h
+++ Anna-Elisabeth/telainicio.h
@@ -1,22 +1,6 @@
 #ifndef TELAINICIO_H
 #define TELAINICIO_H
 
-#include <QDialog>
-
-namespace Ui {
-class telainicio;
-}
-
-class telainicio : public QDialog
-{
-    Q_OBJECT
-
-public:
-    explicit telainicio(QWidget *parent = 0);
-    ~telainicio();
-
-private:
-    Ui::telainicio *ui;
-};
-
-#endif // TELAINICIO_H
+#include <QMainWindow>
+#include <telalogin.h>
+#include <sobre.h>
