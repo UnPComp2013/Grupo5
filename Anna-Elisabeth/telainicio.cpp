387ad5118f8507a3886ed45f29810b2883e4bdcd
annabarata <bebeth_barata@hotmail.com>
2014-11-06 00:58:01 -0200

    Update telainicio.cpp

--- Anna-Elisabeth/telainicio.cpp
+++ Anna-Elisabeth/telainicio.cpp
@@ -1,3 +1,9 @@
 #include "telainicio.h"
 #include "ui_telainicio.h"
 #include <QMessageBox>
+telainicio::telainicio(QWidget *parent) :
+     QDialog(parent),
+    ui(new Ui::telainicio)
+{
+    ui->setupUi(this);
+}
