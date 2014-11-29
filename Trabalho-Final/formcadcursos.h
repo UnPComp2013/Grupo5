7628f55633320decee18901f6eda799d9a44c52e
fernandaagr <fernanda_de_araujo@hotmail.com>
2014-11-29 15:35:44 -0300

    Create formcadcursos.h

---
+++ Trabalho-Final/formcadcursos.h
@@ -0,0 +1,29 @@
+#ifndef FORMCADCURSOS_H
+#define FORMCADCURSOS_H
+
+#include <QDialog>
+
+namespace Ui {
+class formcadcursos;
+}
+
+class formcadcursos : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit formcadcursos(QWidget *parent = 0);
+    ~formcadcursos();
+
+private slots:
+    void on_btCancelar_clicked();
+
+    void on_btLimpar_clicked();
+
+    void on_btSalvar_clicked();
+
+private:
+    Ui::formcadcursos *ui;
+};
+
+#endif // FORMCADCURSOS_H
